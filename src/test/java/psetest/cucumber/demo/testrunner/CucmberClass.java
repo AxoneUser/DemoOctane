1096f8ef2f19cc315cc7b19c17dc7d76db54c8c8
karthik <karthik@192.168.130.154>
2019-08-07 15:07:52 +0100

    Committing the Feature file change

--- src/test/java/psetest/cucumber/demo/testrunner/CucmberClass.java
+++ src/test/java/psetest/cucumber/demo/testrunner/CucmberClass.java
@@ -12,7 +12,7 @@ import cucumber.api.testng.TestNGCucumberRunner;
 
 @CucumberOptions(features = "src/test/resources/features", glue = { "psetest/cucumber/demo/testcases" }, plugin = { "pretty",
 		"html:target/cucumber-reports/cucumber-pretty", "json:target/cucumber-reports/CucumberTestReport.json",
-		"rerun:target/cucumber-reports/re-run.txt" }, monochrome = true, dryRun = false, tags = { "@RegressionTest or @SanityTest" }, strict=true)
+		"rerun:target/cucumber-reports/re-run.txt" }, monochrome = true, dryRun = false, tags = { "@TID4001REV0.3.0" }, strict=true)
 public class CucmberClass {
 	private TestNGCucumberRunner testNGCucumberRunner;
 	
